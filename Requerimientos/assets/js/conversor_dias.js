b49d2788109c3bf2c9a2483e804af27620cc54fc
CamiloLavado <camilolavado@petalmail.com>
2022-02-18 18:16:31 -0300

    desafio terminado

--- Requerimientos/assets/js/conversor_dias.js
+++ Requerimientos/assets/js/conversor_dias.js
@@ -1,14 +1,26 @@
-function tercera() {
+function converdias() {
 
     dias = parseInt(prompt("Ingrese cantidad de dias"))
-    let diferenciaAnos = parseFloat(dias % 365)
-    let diferenciaMeses = parseFloat(diferenciaAnos % 30)
-    let diferenciasemanas = parseFloat(diferenciaMeses % 7)
+    let transfAnios = parseFloat(dias % 365)
+    let transfMeses = parseFloat(transfAnios % 30)
+    let transfSemanas = parseFloat(transfMeses % 7)
 
-    let años = Math.trunc(dias / 365);
-    let meses = Math.trunc(diferenciaAnos / 30);
-    let semanas = Math.trunc(diferenciaMeses / 7)
-    let jou = (diferenciasemanas)
+    let anios = Math.trunc(dias / 365);
+    let meses = Math.trunc(transfAnios / 30);
+    let semanas = Math.trunc(transfMeses / 7)
+    let final = (transfSemanas)
    // Mostrar resultados
 
+    //Inicio del comunicado
+    document.write("<h1>El resultado de las operaciones matematicas es el siguiente</h1><br>");
+//Resta
+document.write("<p>Su cantidad de dias equivale a: " +anios+" Años, "+meses+" meses, "+semanas+" Semanas, "+ final+" Días</p>"+"<br>");
+alert("Su cantidad de dias equivale a: " +anios+" Años, "+meses+" meses, "+semanas+" Semanas, "+ final+" Días");
+console.log("Su cantidad de dias equivale a: " +anios+" Años, "+meses+" meses, "+semanas+" Semanas, "+ final+" Días");
+
+//fin de comunicado
+document.write("<h2>Operaciones terminadas</h2><br>");
 }
+
+//llamamos a la función
+converdias();
--- Requerimientos/assets/js/conversor_temperatura.js
+++ Requerimientos/assets/js/conversor_temperatura.js
@@ -8,15 +8,21 @@ function convertemper() {
     faren = celcius * (9 / 5) + 32
 
 //Mostrar por consola
+
+ //Inicio del comunicado
+ document.write("<h1>El resultado de las operaciones matematicas es el siguiente</h1><br>");
 //Grados Kelvin
 document.write("<p>Su temperatura en grados kelvin es de: " +kelvin +"</p>"+"<br>");
 alert("Su temperatura en grados kelvin es de: " +kelvin);
 console.log("Su temperatura en grados kelvin es de: " +kelvin);
 
 //Grados farenheit
-document.write("<p>Su temperatura en grados kelvin es de: " +faren +"</p>"+"<br>");
-alert("Su temperatura en grados kelvin es de: " +faren);
-console.log("Su temperatura en grados kelvin es de: " +faren);
+document.write("<p>Su temperatura en grados Farenheit es de: " +faren +"</p>"+"<br>");
+alert("Su temperatura en grados Farenheit es de: " +faren);
+console.log("Su temperatura en grados Farenheit es de: " +faren);
+
+//fin de comunicado
+document.write("<h2>Operaciones terminadas</h2><br>");
 
 }
 //llamamos a la funcion
